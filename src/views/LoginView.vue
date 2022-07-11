615b70a0457db45b3e62de0dcc2b3f74f320dbaf
michelle61219@gmail.com <michelle61219@gmail.com>
2022-07-12 00:22:56 +0800

    first commit

---
+++ src/views/LoginView.vue
@@ -0,0 +1,31 @@
+<template>
+    <form @submit.prevent="Submit">
+    <label for ="">Email</label>
+    <input type ="text" v-model="form.email" required>
+    <br><br>
+    <label for=""> Password</label>
+    <input type ="password" v-model ="form.password" required>
+    <br><br>
+    <button type ="submit">Login</button>
+    <br><br>
+    </form>
+</template>
+
+<script>
+export default{
+    data() {
+        return {
+            form:{
+                email:"",
+                password:"",
+            }
+        }
+    },
+
+    methods:{
+        Submit(){
+            this.$store.commit('login', this.form.email)
+        }
+    }
+}
+</script>
